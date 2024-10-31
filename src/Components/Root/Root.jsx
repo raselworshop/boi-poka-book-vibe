6f343e8979ba01392e9c4e74cfa57f13d8db5e89
raselworshop <raselworshop@gmail.com>
2024-10-30 20:55:40 -0400

    added neccessary files

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,10 @@
+
+const Root = () => {
+    return (
+        <div>
+            hello I am from root
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Root from './Components/Root/Root';
+// import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
